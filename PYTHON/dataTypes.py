aa7057fe00d9fe5c7b9a90e3d959257b36ee7e17
Ahmed Al Balochi <a7mad98.work@gmail.com>
2021-02-08 05:38:17 +0500

    basic practice programs written in Python

---
+++ PYTHON/dataTypes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+print(type("My Name is Ahmed"))
+print(type(3))
+print(type(9.8))
+
+####### Type conversion #######
+print(3.5 ,int(3.5))     # "float to int) you notice in this example it only removes the values after int even if they shoule be rounded up
+print("1998" , int("1998")) #"str to int"
+print("1998" , int("1998"))  #+"str to int"
+
+#print(int("23bottles")) doesn't run bcz these in letters in it
+
+print(str(1977))
+print(type(str(1977))) #int to str
+
---
+++ PYTHON/opreators-opreands.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+print(2+4)
+print(5**2) # is like 5 power 2 (5^2)
+print(5*7 - 9*1)
+
+print(4/2)
+print(5/2)
+print(5//2) # removes the values after the point
+print(7 % 3)     # This is the remainder or modulus operator
+
